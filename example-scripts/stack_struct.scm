ea0c74741b2e50b8e8b0ac20e7ce3213919b5077
Matt Paras <matthewparas2020@u.northwestern.edu>
2026-02-04 07:46:59 -0800

    update steel to latest, fix out dated examples

--- example-scripts/stack_struct.scm
+++ example-scripts/stack_struct.scm
@@ -19,30 +19,35 @@
 (define-syntax destruct
   (syntax-rules ()
     [(destruct (var) ret-value)
-     (define (datum->syntax var) (car ret-value))]
+     (define (datum->syntax var)
+       (car ret-value))]
     [(destruct (var1 var2 ...) ret-value)
-     (begin (define (datum->syntax var1) (car ret-value))
-            (destruct (var2 ...) (cdr ret-value)))]))
+     (begin
+       (define (datum->syntax var1)
+         (car ret-value))
+       (destruct (var2 ...) (cdr ret-value)))]))
 
-;; destruct a list
 (define-syntax let-destruct
   (syntax-rules ()
     [(let-destruct ((var) ret-value) body ...)
-     (let ((var (if (null? (cdr ret-value))
+     (let ([var (if (null? (cdr ret-value))
                     (car ret-value)
-                    ret-value)))
-       (begin body ...))]
+                    ret-value)])
+       (begin
+         body ...))]
     [(let-destruct ((var1 var2 ...) ret-value) body ...)
-     (let ((var1 (car ret-value)))
-       (let-destruct ((var2 ...) (cdr ret-value))
-                     body ...))]))
+     (let ([var1 (car ret-value)]) (let-destruct ((var2 ...) (cdr ret-value)) body ...))]))
 
 (define-syntax def-method
   (syntax-rules ()
     [(def-method struct-name (define/method (a this b ...) body ...))
-     (define ((datum->syntax struct-name . a) this b ...)
+     (define ((datum->syntax struct-name |.| a) this b ...)
        (unless ((datum->syntax struct-name ?) this)
-         (error! (datum->syntax struct-name . a) "method takes a value of" struct-name "given" this))
+         (error! (datum->syntax struct-name |.| a)
+                 "method takes a value of"
+                 struct-name
+                 "given"
+                 this))
        body ...)]))
 
 ;; impl block asserts that each function contains the struct type given as the first argument
@@ -52,54 +57,36 @@
 ;; since it does not transform the name of the recursive call
 (define-syntax impl
   (syntax-rules ()
-    [(impl struct-name (define/method (a this b ...) body ...) c ...)
-     (begin (def-method struct-name (define/method (a this b ...) body ...))
-            (impl struct-name c ...))]
     [(impl struct-name (define/method (a this b ...) body ...))
-     (def-method struct-name (define/method (a this b ...) body ...))]))
-
+     (def-method struct-name (define/method (a this b ...) body ...))]
+    [(impl struct-name (define/method (a this b ...) body ...) c ...)
+     (begin
+       (def-method struct-name (define/method (a this b ...) body ...))
+       (impl struct-name c ...))]))
 
 (struct Stack (lst))
-(impl Stack
-      (define/method (pop stack)
-        (define contents (Stack-lst stack))
-        (if (null? contents)
-            '(#f '())
-            (list (car contents) (cdr contents))))
-
-      (define/method (push stack value)
-        (define contents (Stack-lst stack))
-        (Stack (cons value contents))))
+(impl
+ Stack
+ (define/method (pop stack)
+                (define contents (Stack-lst stack))
+                (if (null? contents)
+                    '(#f '())
+                    (list (car contents) (cdr contents))))
+ (define/method (push stack value) (define contents (Stack-lst stack)) (Stack (cons value contents))))
 
 (define test-stack (Stack '()))
 
 (destruct (pop-val-test new-stack-test)
-          (-> test-stack
-               (Stack.push 1)
-               (Stack.push 2)
-               (Stack.push 3)
-               (Stack.push 4)
-               (Stack.pop)))
+          (-> test-stack (Stack.push 1) (Stack.push 2) (Stack.push 3) (Stack.push 4) (Stack.pop)))
 
 pop-val-test ;; => 4
 new-stack-test ;; => '(3 2 1)
 
-
-(let-destruct ((pop-val new-stack)
-               (-> test-stack
-                   (Stack.push 1)
-                   (Stack.push 2)
-                   (Stack.push 3)
-                   (Stack.push 4)
-                   (Stack.pop)))
-
-              (displayln "Hello world")
-              (displayln pop-val)
-              (displayln new-stack))
-
 (let-destruct
- ((a b c)
-  (list 1 2 3 4 5 6 7 8 9 10))
- (displayln a)
- (displayln b)
- (displayln c))
+ ((pop-val new-stack)
+  (-> test-stack (Stack.push 1) (Stack.push 2) (Stack.push 3) (Stack.push 4) (Stack.pop)))
+ (displayln "Hello world")
+ (displayln pop-val)
+ (displayln new-stack))
+
+(let-destruct ((a b c) (list 1 2 3 4 5 6 7 8 9 10)) (displayln a) (displayln b) (displayln c))
--- example-scripts/trie.scm
+++ example-scripts/trie.scm
@@ -8,65 +8,63 @@
 
 ;; Throw in a mediocre flatten definition
 (define (flatten lst)
-  (cond ((null? lst) empty)
-        ((list? lst)
-         (append (flatten (car lst)) (flatten (cdr lst))))
-        (else (list lst))))
+  (cond
+    [(null? lst) empty]
+    [(list? lst) (append (flatten (car lst)) (flatten (cdr lst)))]
+    [else (list lst)]))
 
 ;; contract: (listof char?) (listof tries?) integer? -> (listof trie?)
 (define (create-children char-list lst prefix-chars)
-  (cond [(= (length char-list) 1)
-         (handle-last-letter char-list lst prefix-chars)]
-        [else ;; you are in the middle of the word
-         (handle-intern-letter char-list lst prefix-chars)]))
+  (cond
+    [(= (length char-list) 1) (handle-last-letter char-list lst prefix-chars)]
+    ;; you are in the middle of the word
+    [else (handle-intern-letter char-list lst prefix-chars)]))
 
 ;; contract: (listof char?) (listof trie?) integer? -> (listof trie?)
 (define (handle-last-letter char-list lst prefix-chars)
   (define char (first char-list))
   ; (define next-prefix (append prefix-chars (list char)))
   (define next-prefix (push-back prefix-chars char))
-  (cond [(empty? lst) ;; children are empty, return list of empty children
-         (list (trie char empty #t next-prefix))]
-        [(< char (trie-char (first lst))) ;; less than, put it to the left
-         (cons (trie char empty #t next-prefix) lst)]
-        [(= char (trie-char (first lst))) ;; equal, step down a level
-         (cons (trie char (trie-children (first lst)) #t next-prefix) (rest lst))]
-        [else ;; move to the right
-         (cons (first lst)
-               (create-children char-list (rest lst) prefix-chars))]))
+  (cond
+    ;; children are empty, return list of empty children
+    [(empty? lst) (list (trie char empty #t next-prefix))]
+    ;; less than, put it to the left
+    [(char<? char (trie-char (first lst))) (cons (trie char empty #t next-prefix) lst)]
+    [(char=? char (trie-char (first lst))) ;; equal, step down a level
+     (cons (trie char (trie-children (first lst)) #t next-prefix) (rest lst))]
+    ;; move to the right
+    [else (cons (first lst) (create-children char-list (rest lst) prefix-chars))]))
 
 ;; contract: (listof char?) (listof trie?) integer? -> (listof trie?)
 (define (handle-intern-letter char-list lst prefix-chars)
   (define char (first char-list))
   ; (define next-prefix (append prefix-chars (list char)))
   (define next-prefix (push-back prefix-chars char))
-  (cond [(empty? lst) ;; no children, pop off front and step down
-         (list (trie char (create-children
-                           (rest char-list) empty next-prefix) #f next-prefix))]
-        [(< char (trie-char (first lst))) ;; place where it is, pop off front and go
-         (cons (trie char (create-children
-                           (rest char-list) empty next-prefix) #f next-prefix) lst)]
-        [(= char (trie-char (first lst))) ;; equal, step down
-         (cons (trie char (create-children (rest char-list) (trie-children (first lst)) next-prefix)
-                     (trie-end-word? (first lst))
-                     (trie-word-up-to (first lst)))
-               (rest lst))]
-        [else ; move to the right
-         (cons (first lst)
-               (create-children char-list (rest lst) prefix-chars))]))
+  (cond
+    [(empty? lst) ;; no children, pop off front and step down
+     (list (trie char (create-children (rest char-list) empty next-prefix) #f next-prefix))]
+    [(char<? char (trie-char (first lst))) ;; place where it is, pop off front and go
+     (cons (trie char (create-children (rest char-list) empty next-prefix) #f next-prefix) lst)]
+    [(char=? char (trie-char (first lst))) ;; equal, step down
+     (cons (trie char
+                 (create-children (rest char-list) (trie-children (first lst)) next-prefix)
+                 (trie-end-word? (first lst))
+                 (trie-word-up-to (first lst)))
+           (rest lst))]
+    ; move to the right
+    [else (cons (first lst) (create-children char-list (rest lst) prefix-chars))]))
 
 ;; contract: trie? string? integer? -> trie?
 (define (insert root-trie word)
   (define char-list (string->list word))
-  (trie
-   (trie-char root-trie)
-   (create-children char-list (trie-children root-trie) empty)
-   (trie-end-word? root-trie)
-   (trie-word-up-to root-trie)))
+  (trie (trie-char root-trie)
+        (create-children char-list (trie-children root-trie) empty)
+        (trie-end-word? root-trie)
+        (trie-word-up-to root-trie)))
 
 ; contract: trie? trie? -> boolean?
 (define (trie<? trie-node1 trie-node2)
-  (< (trie-char trie-node1) (trie-char trie-node2)))
+  (char<? (trie-char trie-node1) (trie-char trie-node2)))
 
 ;; contract: trie? -> void
 (define (pre-order-traverse trie-node)
@@ -77,12 +75,8 @@
 ;; contract: trie? (listof string?) -> trie?
 (define (build-trie-from-list-of-words trie list-of-words)
   (cond
-    [(= (length list-of-words) 1)
-     (insert trie (first list-of-words))]
-    [else
-     (build-trie-from-list-of-words
-      (insert trie (first list-of-words))
-      (rest list-of-words))]))
+    [(= (length list-of-words) 1) (insert trie (first list-of-words))]
+    [else (build-trie-from-list-of-words (insert trie (first list-of-words)) (rest list-of-words))]))
 
 ;; ------------------ SORTING ---------------------- ;;
 
@@ -92,29 +86,26 @@
 ;; contract: trie? -> (listof string?)
 (define (pre-order trie-node)
   (if (trie-end-word? trie-node)
-    (cons (list->string (trie-word-up-to trie-node))
-      (flatten (map pre-order (trie-children trie-node))))
-    (flatten (map pre-order (trie-children trie-node)))))
-
+      (cons (list->string (trie-word-up-to trie-node))
+            (flatten (map pre-order (trie-children trie-node))))
+      (flatten (map pre-order (trie-children trie-node)))))
 
 (define test-list
-  (list
-   "suppose"
-   "believe"
-   "changeable"
-   "absent"
-   "busy"
-   "float"
-   "debonair"
-   "throat"
-   "grey"
-   "use"
-   "measure"
-   "van"
-   "thirsty"
-   "notify"
-   "star"
-))
+  (list "suppose"
+        "believe"
+        "changeable"
+        "absent"
+        "busy"
+        "float"
+        "debonair"
+        "throat"
+        "grey"
+        "use"
+        "measure"
+        "van"
+        "thirsty"
+        "notify"
+        "star"))
 
 (define (generate-trie list-of-words)
   (build-trie-from-list-of-words empty-trie list-of-words))
